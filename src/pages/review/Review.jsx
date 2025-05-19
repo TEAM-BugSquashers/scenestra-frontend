1abab01143fb6e6bd7e0882293de83c5f830a1c2
CJW20031104 <kangswak@naver.com>
2025-05-19 09:58:04 +0900

    feat: 리뷰 게시판 #11

--- src/pages/review/Review.jsx
+++ src/pages/review/Review.jsx
@@ -3,7 +3,22 @@ import classes from './Review.module.css'
 function Review(){
     return (
         <>
+            <div className={classes.wrap}>
+                <div className={classes["section_header"]}>
+                    <div></div>
+                    <h2>ABOUT THIS THEATER</h2>
+                    <div></div>
+                </div>
 
+                <section>
+                    <figure className={classes.roomImg}></figure>
+                    <figcaption className={classes["imgDescription"]}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, nulla quod doloribus ex nesciunt repudiandae unde veniam amet id dolore mollitia accusamus voluptatum blanditiis perferendis voluptatibus. Quibusdam voluptatem deleniti vel.
+                    </figcaption>
+                </section>
+
+
+            </div>
         </>
     );
 }
